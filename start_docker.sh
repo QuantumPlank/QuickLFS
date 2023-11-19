8739dec03dbfa83932cb38744adc1af3ea155b3f
QuantumPlank <quantumplank@outlook.com>
2023-11-19 00:12:09 -0600

    More robust docker image handling

--- start_docker.sh
+++ start_docker.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 WORK_DIR=$(pwd)
-IMAGE_NAME=u20lfs
+
 DRIVE=$1
 
-mount $DRIVE /mnt/lfs
+IMAGE_NAME=u20lfs
+IMAGE_SHA=($(sha1sum Docker/Dockerfile))
+IMAGE=$IMAGE_NAME:$IMAGE_SHA
 
-if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+echo "Checking docker image availability"
+if [[ "$(docker images -q $IMAGE 2> /dev/null)" == "" ]]; then
 	echo "Image doesn't exists locally"
 	echo "Building..."
 	cd $WORK_DIR/Docker
-	docker build . -t $IMAGE_NAME
+	docker build . -t $IMAGE
+	cd $WORK_DIR
 fi
 
-cd $WORK_DIR
+echo "Mounting drive"
+sudo mount $DRIVE /mnt/lfs
 
 echo "Starting Docker Container"
-
 docker run \
 	-it \
 	--rm \
 	-v /mnt/lfs:/mnt/lfs  \
-	$IMAGE_NAME \
+	$IMAGE \
 	/bin/bash
